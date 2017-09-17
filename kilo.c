9adcd0e7c300d7da0b9957a47304e90ffb999df3
Justin Barclay <justincbarclay@gmail.com>
2017-09-17 00:01:37 -0400

    Highlight matching string in search

--- kilo.c
+++ kilo.c
@@ -36,7 +36,8 @@ enum editorKey {
 
 enum editorHighlight {
     HL_NORMAL = 0,
-    HL_NUMBER
+    HL_NUMBER,
+    HL_MATCH
 };
 /*** data ***/
 //Editor row, counts size of chars and a buffer of chars
@@ -404,6 +405,7 @@ int getWindowSize(int *rows, int *cols){
 int editorSyntaxToColor(int hl) {
     switch(hl){
     case HL_NUMBER: return 31;
+    case HL_MATCH: return 34;
     default: return 37;
     }
 }
@@ -931,7 +933,15 @@ void editorFindCallback(char *query, int key){
     static int last_match = -1;
     static int direction = 1;
 
+    static int saved_hl_line;
+    static char* saved_hl = NULL;
 
+    if(saved_hl){
+        memcpy(CONFIG.row[saved_hl_line].hl, saved_hl, CONFIG.row[saved_hl_line].rsize);
+        free(saved_hl);
+        saved_hl = NULL;
+    }
+    
     if( key == '\r' || key == '\x1b'){
         last_match = -1;
         direction = 1;
@@ -966,6 +976,11 @@ void editorFindCallback(char *query, int key){
             CONFIG.cy = current;
             CONFIG.cx = editorRowRxToCx(row, match - row->render);
             CONFIG.rowoff = CONFIG.numrows;
+
+            saved_hl_line = current;
+            saved_hl = malloc(row->rsize);
+            memcpy(saved_hl, row->hl, row->rsize);
+            memset(&row->hl[match - row->render], HL_MATCH, strlen(query));
             break;
         }
     }
